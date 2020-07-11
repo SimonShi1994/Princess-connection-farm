6147fa0492030ee509b8ccee5ea3b2c07b44c493
SimonShi1994 <simonshicn@gmail.com>
2020-07-11 11:49:20 +0800

    赛马跳过功能

--- Automator.py
+++ Automator.py
@@ -229,10 +229,25 @@ class Automator:
             self.auth(auth_name=auth_name, auth_id=auth_id)
 
     def init_home(self):
+        # self.lockimg('img/liwu.bmp', elseclick=[(1, 1)], elsedelay=0.5)  # 首页锁定
+        # time.sleep(0.5)
+        # self.lockimg('img/liwu.bmp', elseclick=[(1, 1)], elsedelay=0.2)  # 首页锁定
+        # time.sleep(0.5)
+        while True:
+            screen_shot_ = self.d.screenshot(format='opencv')
+            if UIMatcher.img_where(screen_shot_,'img/jingsaikaishi.bmp'):
+                self.d.click(202,319)  # 第一匹马
+                time.sleep(2)
+                self.d.click(845,495)  # 竞赛开始
+                time.sleep(10)
+            if UIMatcher.img_where(screen_shot_,'img/liwu.bmp'):
+                time.sleep(1)
+                break
+            self.d.click(1,1)
+            time.sleep(0.5)
         self.lockimg('img/liwu.bmp', elseclick=[(1, 1)], elsedelay=0.5)  # 首页锁定
         time.sleep(0.5)
-        self.lockimg('img/liwu.bmp', elseclick=[(1, 1)], elsedelay=0.2)  # 首页锁定
-        time.sleep(0.5)
+
         # 这里防一波第二天可可萝跳脸教程
         screen_shot_ = self.d.screenshot(format='opencv')
         num_of_white, _, _ = UIMatcher.find_gaoliang(screen_shot_)
