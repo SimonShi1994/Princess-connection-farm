ac0ec918f36a3a1d6c37c24dbf6b7a36228291ea
Moment <momincong@foxmail.com>
2020-09-06 04:09:57 +0800

    ocr功能修复

--- api/route/ocr.py
+++ api/route/ocr.py
@@ -2,14 +2,15 @@ from flask import Blueprint, jsonify, request
 
 from core.pcr_config import ocr_mode
 
+# 这一行代码用于关闭tensorflow的gpu模式（如果使用，内存占用翻几倍）
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 if ocr_mode != "网络" and len(ocr_mode) != 0:
     import muggle_ocr
     # 初始化；model_type 包含了 ModelType.OCR/ModelType.Captcha 两种
     sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.OCR)
 
 import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 ocr_api = Blueprint('ocr', __name__)
 
