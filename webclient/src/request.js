cd4ff4704b77b3ac77a62de714cd63507302793a
mkl-kevwang <kevwang@merkleinc.com>
2021-01-19 16:14:04 +0800

    u'p

--- webclient/src/request.js
+++ webclient/src/request.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+const host=``
 export default (url, config = {}) => {
     return axios({
         ...config,
-        url: `http://127.0.0.1:5000/api${url}`,
+        url: `http://${host||'127.0.0.1'}:5000/api${url}`,
     }).then(data => data.data)
 }
