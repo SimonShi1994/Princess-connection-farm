c594ebed7fe3057753b44d85867737ecd3d7e25d
zertu <jsyzzzq@outlook.com>
2021-01-21 13:04:17 +0800

    将schedule页面的数据变为动态获取

--- webclient/src/components/Schedulelist/index.js
+++ webclient/src/components/Schedulelist/index.js
@@ -7,7 +7,7 @@ export default (props) => {
     const { name, record, type, condition, batchlist,index } = props
     return (
         <List.Item> <List.Item.Meta
-            title={<Link to={`/schedule/edit/${index[0]}`}>计划名：{name ? name : '未配置计划名'}</Link>}
+            title={<Link to={`/schedule/edit/${name}`}>计划名：{name ? name : '未配置计划名'}</Link>}
             description={<>{`计划类型：${transformState(type)}`} {condition && condition.start_hour ? `触发时间段：${condition.start_hour}-${condition.end_hour}` : null}{batchlist ? `批配置：${batchlist}` : null} {record ? `记录：${record}` : null}</>}
         /> </List.Item>
     )
--- webclient/src/pages/schedule.js
+++ webclient/src/pages/schedule.js
@@ -1,20 +1,35 @@
-import example from '../example/schedule.json'
 import React from 'react'
 import Schedulelist from '../components/Schedulelist/index'
 import { Collapse, List } from 'antd';
+import request from '../request'
 
 const { Panel } = Collapse;
 export default () => {
+    const [list, setlist] = React.useState([])
+    const [listdata, setdata] = React.useState([])
+    React.useEffect(() => {
+        async function temp() {
+            const { data } = await request('/schedules')
+            setlist(data)
+        }
+        temp();
+    }, [])
+    const getdata = async (index) => {
+        const copy = [...listdata]
+        const { data } = await request(`/get_schedules/${list[index]}`)
+        copy[index] = data.schedules
+        setdata(copy)
+    }
     return (
         <div>
-            <Collapse>
-                {example.map((s, i) => (
-                    <Panel header="This is panel header 1" key={i}>
+            <Collapse onChange={getdata} accordion>
+                {list.map((s, i) => (
+                    <Panel header={s} key={i}>
                         <List
                             size="large"
                             bordered
-                            dataSource={s.schedules}
-                            renderItem={(item, index) => <Schedulelist {...item} index={[i,index]} />}
+                            dataSource={listdata[i] || []}
+                            renderItem={(item, index) => <Schedulelist {...item} index={[i, index]} />}
                         />
                     </Panel>
                 ))}
