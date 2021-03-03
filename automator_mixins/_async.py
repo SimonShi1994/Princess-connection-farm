92b6713ca545129227c8aea8a01aab38b6ba7a3c
CyiceK <klvrw@outlook.com>
2021-03-03 15:41:11 +0800

    优化BOT发送图片的方式

--- automator_mixins/_async.py
+++ automator_mixins/_async.py
@@ -224,8 +224,9 @@ class AsyncMixin(ToolsMixin):
         :return:
         """
         while Multithreading({}).is_stopped():
-            await asyncio.sleep(sentstate * 60 + 1)
-            pcr_log(self.account).server_bot('', '', '', img=self.last_screen, img_title=f"server_bot运行截图播报\n"
+            if sentstate != 0:
+                await asyncio.sleep(sentstate * 60 + 1)
+                pcr_log(self.account).server_bot('', '', '', img=self.last_screen, img_title=f"server_bot运行截图播报\n"
                                                                                          f"账号:{self.account}\n"
                                                                                          f"所运行的设备:{self.address}")
 
