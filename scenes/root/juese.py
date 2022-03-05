5a4644fd34e5442a2b5ab5ad98702c86f707e7ae
UVJkiNTQ <lxdluke1234@gmail.com>
2022-03-05 23:40:27 +0800

    typo

--- scenes/root/juese.py
+++ scenes/root/juese.py
@@ -204,7 +204,7 @@ class CharBase(SevenBTNMixin):
                     if not self.is_exists(JUESE_BTN["ticked"].img, at=(362, 330, 416, 385)):
                         self.click(386, 353)  # 勾选
                         self.click(589, 425)  # 确认
-                self.lock_img(JUESE_BTN["juqingjiesuo"], aat=(429, 22, 525, 257))
+                self.lock_img(JUESE_BTN["juqingjiesuo"], at=(429, 22, 525, 257))
                 self.click_img(JUESE_BTN["guanbi"])  # 关闭
             self.lock_img(JUESE_BTN["juesejuqing"])
             self.log.write_log("info", "无法再提升好感度")
