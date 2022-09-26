3dc5e46d7ed3671f9b4b214c5a38b50235a5baba
UVJkiNTQ <12570945+UVJkiNTQ@users.noreply.github.com>
2022-09-26 23:48:30 +0800

    Update constant.py

--- core/constant.py
+++ core/constant.py
@@ -453,7 +453,7 @@ HANGHUI_BTN = {
     "kkr_dialog": p(280, 402, img="img/hanghui/battle/kkr_dialog.bmp", at=(257, 392, 304, 412)),  # kkr对话框设别
     "kkr_dialog2": p(281, 401, img="img/hanghui/battle/kkr_dialog2.bmp", at=(255, 392, 307, 411)),  # kkr对话框设别
     "queren": p(477, 477, img="img/hanghui/battle/queren.bmp", at=(455, 466, 499, 488)),  # 确认报酬
-    "tdzksqr": p(478, 144, img="img/hanghui/battle/tdzksqr.bmp", at=(403, 132, 553, 157)),  # 团队战开始确认
+    "tdzksqr": p(478, 144, img="img/hanghui/battle/tdzksqr.bmp", at=(401, 132, 556, 160)),  # 团队战开始确认
     "fhsjqr": p(478, 42, img="img/hanghui/battle/tdzksqr.bmp", at=(401, 28, 556, 56)),  # 返还时间开始确认
     "zhandou_confirm": p(587, 370, img="img/hanghui/battle/zhandou_confirm.bmp", at=(559, 354, 616, 386)),  # 团队战开始(战斗)
     "zhandou_confirm2": p(587, 478, img="img/hanghui/battle/zhandou_confirm.bmp", at=(558, 462, 616, 495)),  # 团队战开始(战斗)
