a06feb69db19944c73a82f5cf839a759b5733b69
UVJkiNTQ <12570945+UVJkiNTQ@users.noreply.github.com>
2022-09-27 20:26:26 +0800

    Ver0927

--- main_new.py
+++ main_new.py
@@ -39,7 +39,7 @@ import cv2
 PCR: Optional[PCRInitializer] = None
 SCH: Optional[Schedule] = None
 last_schedule = ""
-script_version = "Ver 2.8.20220919.3"
+script_version = "Ver 2.8.20220927"
 
 
 def GetLastSchedule():
