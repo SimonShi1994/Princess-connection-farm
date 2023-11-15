3d27c0176631f261ce07fa42eb43038646305f09
cxf1113w3 <67044453+cxf1113w3@users.noreply.github.com>
2023-11-16 00:50:09 +0800

    Ver2.8.20231115

--- core/constant.py
+++ core/constant.py
@@ -1392,6 +1392,7 @@ ZHUXIAN_ID = {
     49: p(img="img/zhuxian/49L.bmp", at=(61, 59, 179, 75)),
     50: p(img="img/zhuxian/50L.bmp", at=(62, 59, 161, 76)),
     51: p(img="img/zhuxian/51L.bmp", at=(62, 59, 157, 76)),
+    52: p(img="img/zhuxian/52L.bmp", at=(62, 57, 181, 77)),
 }
 
 MAX_MAP = max(ZHUXIAN_ID)
@@ -1467,6 +1468,9 @@ ZHUXIAN_NLYJ_ID = {
     50: p(img="img/zhuxian/50R.bmp", at=(179, 58, 215, 76)),
     51: p(img="img/zhuxian/51R.bmp", at=(175, 58, 211, 76)),
 }
+ZHUXIAN_PNATJ_ID = {
+    52: p(img="img/zhuxian/52R.bmp", at=(195, 59, 233, 79)),
+}
 ZHUXIAN_SECOND_ID = {
     (18, 19): ZHUXIAN_XXXYY_ID,
     (20, 21): ZHUXIAN_KSTLYSL_ID,
@@ -1485,6 +1489,7 @@ ZHUXIAN_SECOND_ID = {
     (46, 47): ZHUXIAN_LTLQS_ID,
     (48, 49): ZHUXIAN_ASTHY_ID,
     (50, 51): ZHUXIAN_NLYJ_ID,
+    (52, ): ZHUXIAN_PNATJ_ID,
 }
 
 NORMAL_COORD = {
